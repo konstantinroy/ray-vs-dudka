d2e70be5ed6c1733da69f0e29a6dedddfec6a8f3
KonstantinRoy9977 <konstantin.roy9977@gmail.com>
2022-12-07 22:35:02 +0300

    adding audiosarray

---
+++ src/components/AudioPage/AudiosArray.js
@@ -0,0 +1,204 @@
+import One from "./audio/1. Заключительный разговор Рая и Дудки.mp3";
+import Two from "./audio/2. Райская месть.mp3";
+import Three from "./audio/3. Расплата.mp3";
+import Four from "./audio/4. Предатель должен умереть.mp3";
+import Five from "./audio/5. Талантливый Никита Дудка.mp3";
+import Six from "./audio/6. Мысли Дудки.mp3";
+import Seven from "./audio/7. Наемник-Дудка.mp3";
+import Eight from "./audio/8. Фифа.mp3";
+import Nine from "./audio/9. Откровение Дудки.mp3";
+import Ten from "./audio/10. День твоей смерти.mp3";
+import Eleven from "./audio/11. Армия.mp3";
+import Twelwe from "./audio/12. Он забрал мою душу.mp3";
+import Thirteen from "./audio/13. Разговор Никиты со своим вторым Я.mp3";
+import Fourteen from "./audio/14. Дружба Кости и Никиты.mp3";
+import Fiveteen from "./audio/15. Раскаяние Рая.mp3";
+import Sixteen from "./audio/16. Nikita Dudka best moments comp.mp3";
+import Seventeen from "./audio/17. НРЖ.mp3";
+import Eighteen from "./audio/18. Никита на приёме у психоаналитика.mp3";
+import Nineteen from "./audio/19. Интервью Никиты Дудки после победы в финале Чемпионата мира по фифе.mp3";
+import Twenteen from "./audio/20. Переговоры Рая и Куца.mp3";
+import Twentyone from "./audio/21. Поздравление Никиты с днём рождения.mp3";
+import Twentytwo from "./audio/22. Никита после того, как впервые за долгое время проиграл Косте в фифу.mp3";
+import Twentythree from "./audio/23. Никита «не орёт», когда играет в фифу.mp3";
+import Twentyfour from "./audio/24. Welcome to the party.mp3";
+import Twentyfive from "./audio/25. Как Никита придумывает правила игры в фифу....mp3";
+import Twentysix from "./audio/26. Хорошее настроение Никиты после победы над Костей.mp3";
+import Twentyseven from "./audio/27. no pain no gain.wav";
+import Twentyeight from "./audio/28. Молитва Никиты.mp3";
+import Twentynine from "./audio/29. Никита Дудка - Песня про фифу.mp3";
+import Thirty from "./audio/30. Рекламный ролик Матч тв к сегодняшнему матчу Рая и Дудки.mp3";
+import Thirtyone from "./audio/31. Разговор Никиты с Максом.mp3";
+import Thirtytwo from "./audio/32. Каким будет день, когда Никита починит xbox.mp3";
+import Thirtythree from "./audio/33. Дуда сообщает Марку о результате катки.mp3";
+
+const AudiosArray = [
+  {
+    id: 1,
+    audio_name: "Заключительный разговор Рая и Дудки",
+    audio_link: One,
+  },
+  { 
+    id: 2, 
+    audio_name: "Райская месть", 
+    audio_link: Two 
+  },
+  { 
+    id: 3, 
+    audio_name: "Расплата", 
+    audio_link: Three },
+  {
+    id: 4,
+    audio_name: "Предатель должен умереть",
+    audio_link: Four,
+  },
+  {
+    id: 5,
+    audio_name: "Талантливый Никита Дудка",
+    audio_link: Five,
+  },
+  {
+    id: 6,
+    audio_name: "Мысли Дудки",
+    audio_link: Six,
+  },
+  {
+    id: 7,
+    audio_name: "Наемник-Дудка",
+    audio_link: Seven,
+  },
+  {
+    id: 8,
+    audio_name: "Фифа",
+    audio_link: Eight,
+  },
+  {
+    id: 9,
+    audio_name: "Откровение Дудки",
+    audio_link: Nine,
+  },
+  {
+    id: 10,
+    audio_name: "День твоей смерти",
+    audio_link: Ten,
+  },
+  {
+    id: 11,
+    audio_name: "Армия",
+    audio_link: Eleven,
+  },
+  {
+    id: 12,
+    audio_name: "Он забрал мою душу",
+    audio_link: Twelwe,
+  },
+  {
+    id: 13,
+    audio_name: "Разговор Никиты со своим вторым Я",
+    audio_link: Thirteen,
+  },
+  {
+    id: 14,
+    audio_name: "Дружба Кости и Никиты",
+    audio_link: Fourteen,
+  },
+  {
+    id: 15,
+    audio_name: "Раскаяние Рая",
+    audio_link: Fiveteen,
+  },
+  {
+    id: 16,
+    audio_name: "Nikita Dudka best moments comp",
+    audio_link: Sixteen,
+  },
+  {
+    id: 17,
+    audio_name: "НРЖ",
+    audio_link: Seventeen,
+  },
+  {
+    id: 18,
+    audio_name: "Никита на приёме у психоаналитика",
+    audio_link: Eighteen,
+  },
+  {
+    id: 19,
+    audio_name:
+      "Интервью Никиты Дудки после победы в финале Чемпионата мира по фифе",
+    audio_link: Nineteen,
+  },
+  {
+    id: 20,
+    audio_name: "Переговоры Рая и Куца",
+    audio_link: Twenteen,
+  },
+  {
+    id: 21,
+    audio_name: "Поздравление Никиты с днём рождения",
+    audio_link: Twentyone,
+  },
+  {
+    id: 22,
+    audio_name:
+      "Никита после того, как впервые за долгое время проиграл Косте в фифу",
+    audio_link: Twentytwo,
+  },
+  {
+    id: 23,
+    audio_name: "Никита «не орёт», когда играет в фифу",
+    audio_link: Twentythree,
+  },
+  {
+    id: 24,
+    audio_name: "Welcome to the party",
+    audio_link: Twentyfour,
+  },
+  {
+    id: 25,
+    audio_name: "Как Никита придумывает правила игры в фифу...",
+    audio_link: Twentyfive,
+  },
+  {
+    id: 26,
+    audio_name: "Хорошее настроение Никиты после победы над Костей",
+    audio_link: Twentysix,
+  },
+  {
+    id: 27,
+    audio_name: "no pain no gain",
+    audio_link: Twentyseven,
+  },
+  {
+    id: 28,
+    audio_name: "Молитва Никиты",
+    audio_link: Twentyeight,
+  },
+  {
+    id: 29,
+    audio_name: "Никита Дудка - Песня про фифу",
+    audio_link: Twentynine,
+  },
+  {
+    id: 30,
+    audio_name: "Рекламный ролик Матч тв к сегодняшнему матчу Рая и Дудки",
+    audio_link: Thirty,
+  },
+  {
+    id: 31,
+    audio_name: "Разговор Никиты с Максом",
+    audio_link: Thirtyone,
+  },
+  {
+    id: 32,
+    audio_name: "Каким будет день, когда Никита починит xbox",
+    audio_link: Thirtytwo,
+  },
+  {
+    id: 33,
+    audio_name: "Дуда сообщает Марку о результате катки",
+    audio_link: Thirtythree,
+  },
+];
+
+export default AudiosArray;
